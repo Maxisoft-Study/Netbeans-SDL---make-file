3531c2add3b0d32e6aa22e322e3fda4806036829
maxisoft <maxisoft.eu@gmail.com>
2013-03-17 13:26:47 +0100

    init

---
+++ main.c
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.c
+ * Author: i5
+ *
+ * Created on 16 mars 2013, 07:31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+
+
+void pause();
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
+    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    pause(); // Mise en pause du programme
+    SDL_Quit(); // Arrêt de la SDL
+    return (EXIT_SUCCESS);
+}
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+  
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
+
